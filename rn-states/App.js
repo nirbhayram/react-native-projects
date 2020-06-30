057e40638ffdece3c29e4091646450d3350f3423
Nirbhay <da201501068@gmail.com>
2020-06-30 23:30:40 +0530

    states management simple application built

--- rn-states/App.js
+++ rn-states/App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: "#FFFFFF",
+      flex: 1,
+      paddingTop: 50,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    textInput: {
+      fontSize: 20,
+      color: "#ffffff",
+      paddingHorizontal:20,
+      paddingVertical:10,
+      borderColor:"#ffffff",
+      backgroundColor: "#7EC4CF",
+      borderRadius: 10
+    },
+    textStyle:{
+      marginTop:30,
+      fontSize:20
+    }
+  })
+
+  const textChanged=(text)=>{
+    setText(text)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        placeholderTextColor="#ffffff"
+        placeholder="Please enter text here"
+        style={styles.textInput}
+        onChangeText={(text)=>textChanged(text)}
+      />
+      <Text style={styles.textStyle}>{text.split(' ').map(text => text && '😍').join('')}</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
