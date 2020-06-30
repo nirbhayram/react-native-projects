b579d37fce2da5420a1962d656c5f185c2d38bae
Nirbhay <da201501068@gmail.com>
2020-07-01 00:13:43 +0530

    Added random backgorund color change

--- rn-states/App.js
+++ rn-states/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, TextInput } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
 export default function App() {
   const [text, setText] = useState("");
+  const [randomColor, setRandomColor] = useState("")
 
   const styles = StyleSheet.create({
     container: {
@@ -15,31 +16,47 @@ export default function App() {
     textInput: {
       fontSize: 20,
       color: "#ffffff",
-      paddingHorizontal:20,
-      paddingVertical:10,
-      borderColor:"#ffffff",
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      borderColor: "#ffffff",
       backgroundColor: "#7EC4CF",
       borderRadius: 10
     },
-    textStyle:{
-      marginTop:30,
-      fontSize:20
+    textStyle: {
+      marginTop: 30,
+      fontSize: 20
     }
   })
 
-  const textChanged=(text)=>{
+  const textChanged = (text) => {
     setText(text)
   }
 
+  const changeBackGorundColor=()=>{
+    setRandomColor(getRandomBackGroundColor())
+  }
+
+  const getRandomBackGroundColor=()=>{
+    return "rgb("+
+      Math.floor(Math.random()*255)+
+      ","+
+      Math.floor(Math.random()*255)+
+      ","+
+      Math.floor(Math.random()*255)+
+      ")"
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor:randomColor}]}>
       <TextInput
         placeholderTextColor="#ffffff"
         placeholder="Please enter text here"
         style={styles.textInput}
-        onChangeText={(text)=>textChanged(text)}
+        onChangeText={(text) => textChanged(text)}
       />
-      <Text style={styles.textStyle}>{text.split(' ').map(text => text && '😍').join('')}</Text>
+      <TouchableOpacity onPress={()=>{changeBackGorundColor()}}>
+        <Text style={styles.textStyle}>{text.split(' ').map(text => text && '😍').join('')}</Text>
+      </TouchableOpacity>
     </View>
   )
 }
