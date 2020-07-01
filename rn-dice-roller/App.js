22321bd6d83aed869ef2a30b575fcebe57fb02c5
Nirbhay <da201501068@gmail.com>
2020-07-01 18:42:36 +0530

    dice roller app completed

--- rn-dice-roller/App.js
+++ rn-dice-roller/App.js
@@ -1,19 +1,38 @@
-
 import React, { useState } from 'react';
-import { StyleSheet, SafeAreaView, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, SafeAreaView, Image, TouchableOpacity, Alert } from 'react-native';
 
 export default function App() {
-  const [imagevariable, setImagevariable] = useState("1");
+  const [imagevariable1, setImagevariable1] = useState(require("./src/images/dice1.png"));
+  const [imagevariable2, setImagevariable2] = useState(require("./src/images/dice1.png"));
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 6 + 1)
+  }
 
   const rollTheDice = () => {
-    //setImagevariable(Math.floor(Math.random() * 5 + 1).toString)
+    switch (getRandomNumber()) {
+      case 1:
+        return require("./src/images/dice1.png");
+      case 2:
+        return require("./src/images/dice2.png");
+      case 3:
+        return require("./src/images/dice3.png");
+      case 4:
+        return require("./src/images/dice4.png");
+      case 5:
+        return require("./src/images/dice5.png");
+      case 6:
+        return require("./src/images/dice6.png");
+      default:
+        break;
+    }
   }
 
   return (
-
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity onPress={()=>rollTheDice()}>
-        <Image source={require("./src/images/dice1.png")}></Image>
+      <TouchableOpacity onPress={() => {setImagevariable1(rollTheDice);setImagevariable2(rollTheDice)}}>
+        <Image source={imagevariable1}></Image>
+        <Image source={imagevariable2}></Image>
       </TouchableOpacity>
     </SafeAreaView>
   );
@@ -24,6 +43,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
   },
 });
