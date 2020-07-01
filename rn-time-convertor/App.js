d2df09aa09fd7682399c326a4815da579f70e527
Nirbhay <da201501068@gmail.com>
2020-07-01 21:59:25 +0530

    date and time picker set

--- rn-time-convertor/App.js
+++ rn-time-convertor/App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React, { useState } from "react";
+import { Button, View, SafeAreaView } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+ 
+const App = () => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+ 
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+ 
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+ 
+  const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    hideDatePicker();
+  };
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{paddingTop:30}}>
+      <Button title="Show Date Picker" onPress={showDatePicker} />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="datetime"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+        headerTextIOS="Pick a date and time"
+      />
+    </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+ 
+export default App;
