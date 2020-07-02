e98d0573de582f95d2059f040a086ea725183c31
Nirbhay <da201501068@gmail.com>
2020-07-03 00:04:01 +0530

    complete the logic part

--- rn-time-convertor/src/package/TimeConvert.js
+++ rn-time-convertor/src/package/TimeConvert.js
@@ -3,15 +3,15 @@ const TIME_ZONE = {
         offset: 0
     },
     GMT: {
-        offset: (-1)*(((5*60)+60)*60*1000)
+        offset: (-1)*(((5*60)+30)*60*1000)
     },
     HKT: {
-        offset: ((2*60)+60)*60*1000
+        offset: ((2*60)+30)*60*1000
     }
 }
 
 const getAllTime = (_timeZone, _date, _wanted=['IST','GMT','HKT'])=>{
-    console.log(_date)
+    console.log(_date.getTime())
     const _retObject = new Map();
     _wanted.map((timeZone)=>{
         // console.log(`${TIME_ZONE[timeZone]['offset']} ${TIME_ZONE[_timeZone]['offset']}`)
@@ -23,8 +23,9 @@ const getAllTime = (_timeZone, _date, _wanted=['IST','GMT','HKT'])=>{
 }
 
 const addOffsetToDate = (_date,_offset)=>{
-    // console.log(_date+" "+_offset+"*******")
+    console.log(_date+" "+_offset+"*******")
     return new Date(_date.getTime()+_offset);
 }
 
-console.log( getAllTime('IST',new Date()))
+let times = getAllTime('IST',new Date())
+times.forEach((value,key)=>{console.log(`${key} ${value}`)})
