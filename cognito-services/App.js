7ff176af705636f3e43c79436a485389d897d86f
Nirbhay <da201501068@gmail.com>
2020-07-07 23:09:12 +0530

    Added authentication flow of congnito service

--- cognito-services/App.js
+++ cognito-services/App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws_config';
 Amplify.configure(awsconfig);
+import { withAuthenticator } from 'aws-amplify-react-native'
 
-export default function App() {
+App =() =>{
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -22,3 +23,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(App)
