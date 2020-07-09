7ac7234c7c4d607e25fff4df5034433bbdb5d731
Nirbhay <da201501068@gmail.com>
2020-07-09 23:54:33 +0530

    learning some styling

--- tic-tac-toe/App.js
+++ tic-tac-toe/App.js
@@ -5,12 +5,17 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-        <View style={[styles.rowView, { backgroundColor: "#6D466B" }]}>
-        </View>
-        <View style={[styles.rowView, { backgroundColor: "#412234" }]}>
-        </View>
-        <View style={[styles.rowView, { backgroundColor: "#ffffff" }]}>
-        </View>
+      <View style={[styles.rowView, { backgroundColor: "#6D466B" }]} onLayout={(event) => {
+        var { x, y, width, height } = event.nativeEvent.layout; console.log(height);
+      }}>
+        <View style={{backgroundColor:"#ffffff", width:81.5, height:"100%"}}></View>
+        <View style={{backgroundColor:"#412234", width:81.5, height:"100%"}}></View>
+        <View style={{backgroundColor:"#ffffff", width:81.5, height:"100%"}}></View>
+      </View>
+      <View style={[styles.rowView, { backgroundColor: "#412234" }]}>
+      </View>
+      <View style={[styles.rowView, { backgroundColor: "#ffffff" }]}>
+      </View>
     </SafeAreaView>
   );
 }
@@ -24,8 +29,8 @@ const styles = StyleSheet.create({
   },
   rowView: {
     // flex: 1,
-    height:"10%",
-    width:"100%",
+    height: "10%",
+    width: "100%",
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center"
