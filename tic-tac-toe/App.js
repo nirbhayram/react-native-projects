4d7c511fa07237ff558dd57a0c1db227247c7b1e
Nirbhay <da201501068@gmail.com>
2020-07-10 00:07:08 +0530

    adjusting width respect to height

--- tic-tac-toe/App.js
+++ tic-tac-toe/App.js
@@ -1,16 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+
+  const [widthOfBox, setWidthOfBox] = useState(81.5);
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#B49FCC',
+    },
+    rowView: {
+      // flex: 1,
+      height: "11%",
+      width: "100%",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    box: {
+      backgroundColor: "#ffffff",
+      width: widthOfBox,
+      height: "100%"
+    }
+  });
+
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={[styles.rowView, { backgroundColor: "#6D466B" }]} onLayout={(event) => {
-        var { x, y, width, height } = event.nativeEvent.layout; console.log(height);
-      }}>
-        <View style={{backgroundColor:"#ffffff", width:81.5, height:"100%"}}></View>
-        <View style={{backgroundColor:"#412234", width:81.5, height:"100%"}}></View>
-        <View style={{backgroundColor:"#ffffff", width:81.5, height:"100%"}}></View>
+      <View style={[styles.rowView, { backgroundColor: "#6D466B" }]} onLayout={
+        (event) => {
+          var { x, y, width, height } = event.nativeEvent.layout;
+          setWidthOfBox(height)
+          console.log(height);
+        }}
+      >
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
       </View>
       <View style={[styles.rowView, { backgroundColor: "#412234" }]}>
       </View>
@@ -19,20 +49,3 @@ export default function App() {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#B49FCC',
-  },
-  rowView: {
-    // flex: 1,
-    height: "10%",
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
