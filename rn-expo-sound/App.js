ec6ac33b99f6ce86f86692402dea3a6b90e08ad6
Nirbhay <da201501068@gmail.com>
2020-07-12 23:05:16 +0530

    sound play functionality added

--- rn-expo-sound/App.js
+++ rn-expo-sound/App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import { Audio } from 'expo-av';
 
 export default function App() {
+
+  const playSound = async (number) => {
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync(require('./assets/four.wav'));
+      await soundObject.playAsync().then(async (playBackStatus) => {
+        setTimeout(() => {
+
+        }, playBackStatus.playableDurationMillis());
+        soundObject.unloadAsync()
+      });
+      // Your sound is playing!
+    } catch (error) {
+      // An error occurred!
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>audio</Text>
+      <TouchableOpacity onPress={() => { playSound() }} >
+        <Text>audio</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
