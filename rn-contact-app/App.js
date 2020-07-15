574236ddb5ee6c29832d748047d898bf75d1c2e9
Nirbhay <da201501068@gmail.com>
2020-07-15 21:53:56 +0530

    added all the basic screen

--- rn-contact-app/App.js
+++ rn-contact-app/App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native"
+import {createStackNavigator} from "@react-navigation/stack"
+
+import AddContactScreen from "./src/screen/AddContactScreen";
+import HomeScreen from "./src/screen/HomeScreen";
+import ViewContactScreen from "./src/screen/ViewContactScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Add" component={AddContactScreen} />
+        <Stack.Screen name="View" component={ViewContactScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
