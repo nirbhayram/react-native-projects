9e2e81cb40f956221b2818ca60f8f5da23b6e13b
Nirbhay <da201501068@gmail.com>
2020-07-15 22:24:25 +0530

    added float button

--- rn-contact-app/src/screen/HomeScreen.js
+++ rn-contact-app/src/screen/HomeScreen.js
@@ -1,19 +1,67 @@
 import React from 'react'
-import { View,Text,StyleSheet } from 'react-native'
+import { View,Text,StyleSheet, TouchableOpacity, FlatList } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage';
+import { AntDesign } from '@expo/vector-icons'; 
 
-export default function HomeScreen() {
+export default function HomeScreen({route, navigation}) {
     return (
         <View style={styles.container}>
-            <Text>Home screen</Text>
+            <TouchableOpacity
+            style={styles.floatButton} 
+            onPress={()=>{
+                navigation.navigate("Add")
+            }}>
+                <AntDesign 
+                name="plus" 
+                size={30} 
+                color="white" />
+            </TouchableOpacity>
         </View>
     )
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
+  listItem: {
+    flexDirection: "row",
+    padding: 20
+  },
+  iconContainer: {
+    width: 50,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#B83227",
+    borderRadius: 100
+  },
+  contactIcon: {
+    fontSize: 28,
+    color: "#fff"
+  },
+  infoContainer: {
+    flexDirection: "column"
+  },
+  infoText: {
+    fontSize: 16,
+    fontWeight: "400",
+    paddingLeft: 10,
+    paddingTop: 2
+  },
+  floatButton: {
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 60,
+    position: "absolute",
+    bottom: 40,
+    right: 10,
+    height: 60,
+    backgroundColor: "#B83227",
+    borderRadius: 100
+  }
 });
