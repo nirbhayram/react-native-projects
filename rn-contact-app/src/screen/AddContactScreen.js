b4ebbeafd71438bb64c6f718efc1190c75299016
Nirbhay <da201501068@gmail.com>
2020-07-15 22:44:16 +0530

    added save method

--- rn-contact-app/src/screen/AddContactScreen.js
+++ rn-contact-app/src/screen/AddContactScreen.js
@@ -1,7 +1,65 @@
-import React from 'react'
-import { View,Text,StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    Keyboard,
+    TouchableWithoutFeedback,
+    ScrollView,
+    Alert
+} from 'react-native'
 
-export default function AddContactScreen() {
+import {
+    Form,
+    Item,
+    Input,
+    Label,
+    Button
+} from "native-base";
+
+import AsyncStorage from '@react-native-community/async-storage';
+
+export default function AddContactScreen({ route, navigation }) {
+
+    const [state, setState] = useState({
+        fname: "",
+        lname: "",
+        phone: "",
+        email: "",
+        address: ""
+    })
+
+    const saveContact = async () => {
+        if (
+            state[fname] !== "" &&
+            state[lname] !== "" &&
+            state[phone] !== "" &&
+            state[email] !== "" &&
+            state[address] !== ""
+        ) {
+            const contact = {
+                fname: state[fname],
+                lname: state[lname],
+                phone: state[phone],
+                email: state[email],
+                address: state[address]
+            }
+            await AsyncStorage.setItem(
+                new Date().toString(),
+                JSON.stringify(contact)
+            ).then(
+                navigation.goBack()
+            ).catch(
+                error=>{
+                    console.warn(error)
+                }
+            )
+
+        } else {
+            Alert.alert("All the fields are required!")
+        }
+    }
+
     return (
         <View style={styles.container}>
             <Text>Add contact screen</Text>
@@ -10,10 +68,10 @@ export default function AddContactScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
