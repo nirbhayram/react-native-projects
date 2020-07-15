ca571965b303a4ad30538d02a9580cdcbcf9373b
Nirbhay <da201501068@gmail.com>
2020-07-15 23:46:56 +0530

    saving in local storage

--- rn-contact-app/src/screen/AddContactScreen.js
+++ rn-contact-app/src/screen/AddContactScreen.js
@@ -1,20 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import {
     View,
-    Text,
     StyleSheet,
     Keyboard,
     TouchableWithoutFeedback,
     ScrollView,
     Alert
 } from 'react-native'
+import * as Font from 'expo-font';
 
 import {
+    Text,
     Form,
     Item,
     Input,
     Label,
-    Button
+    Textarea,
+    Button,
+    Spinner
 } from "native-base";
 
 import AsyncStorage from '@react-native-community/async-storage';
@@ -29,20 +32,34 @@ export default function AddContactScreen({ route, navigation }) {
         address: ""
     })
 
+    const [loading, setLoading] = useState(true)
+
+    const loadFont = async () => {
+        await Font.loadAsync({
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+        })
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        loadFont()
+    }, [])
+
     const saveContact = async () => {
         if (
-            state[fname] !== "" &&
-            state[lname] !== "" &&
-            state[phone] !== "" &&
-            state[email] !== "" &&
-            state[address] !== ""
+            state["fname"] !== "" &&
+            state["lname"] !== "" &&
+            state["phone"] !== "" &&
+            state["email"] !== "" &&
+            state["address"] !== ""
         ) {
             const contact = {
-                fname: state[fname],
-                lname: state[lname],
-                phone: state[phone],
-                email: state[email],
-                address: state[address]
+                fname: state["fname"],
+                lname: state["lname"],
+                phone: state["phone"],
+                email: state["email"],
+                address: state["address"]
             }
             await AsyncStorage.setItem(
                 new Date().toString(),
@@ -50,7 +67,7 @@ export default function AddContactScreen({ route, navigation }) {
             ).then(
                 navigation.goBack()
             ).catch(
-                error=>{
+                error => {
                     console.warn(error)
                 }
             )
@@ -60,18 +77,94 @@ export default function AddContactScreen({ route, navigation }) {
         }
     }
 
-    return (
-        <View style={styles.container}>
-            <Text>Add contact screen</Text>
-        </View>
-    )
+    if (loading) {
+        return(
+            <Spinner />
+        )
+    } else {
+        return (
+            <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss() }}>
+                <ScrollView style={styles.container}>
+                    <Form>
+                        <Item style={styles.inputItem}>
+                            <Label>First name</Label>
+                            <Input
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                keyboardType="default"
+                                onChangeText={fname => { setState({ fname }) }}
+                            />
+                        </Item>
+                        <Item style={styles.inputItem}>
+                            <Label>Last name</Label>
+                            <Input
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                keyboardType="default"
+                                onChangeText={lname => { setState({ lname }) }}
+                            />
+                        </Item>
+                        <Item style={styles.inputItem}>
+                            <Label>Phone no</Label>
+                            <Input
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                keyboardType="default"
+                                onChangeText={phone => { setState({ phone }) }}
+                            />
+                        </Item>
+                        <Item style={styles.inputItem}>
+                            <Label>email</Label>
+                            <Input
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                keyboardType="default"
+                                onChangeText={email => { setState({ email }) }}
+                            />
+                        </Item>
+
+                        <Textarea
+                            style={styles.inputItem}
+                            bordered
+                            rowSpan={4}
+                            placeholder="Address"
+                            onChangeText={(address) => { setState({ address }) }} />
+
+                    </Form>
+
+                    <Button style={styles.button} onPress={() => { saveContact() }}
+                        full
+                    >
+                        <Text style={styles.buttonText}>Save</Text>
+                    </Button>
+
+                </ScrollView>
+            </TouchableWithoutFeedback>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: "#fff",
+        margin: 10,
+        height: 500
     },
+    inputItem: {
+        margin: 10
+    },
+    button: {
+        alignSelf: "center",
+        backgroundColor: "#B83227",
+        marginTop: 40
+    },
+    buttonText: {
+        color: "#fff",
+        fontWeight: "bold"
+    },
+    empty: {
+        height: 500,
+        backgroundColor: "#FFF"
+    }
 });
--- rn-contact-app/src/screen/HomeScreen.js
+++ rn-contact-app/src/screen/HomeScreen.js
@@ -1,67 +1,83 @@
 import React from 'react'
-import { View,Text,StyleSheet, TouchableOpacity, FlatList } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage';
-import { AntDesign } from '@expo/vector-icons'; 
+import { AntDesign } from '@expo/vector-icons';
+import { Button } from 'native-base';
 
-export default function HomeScreen({route, navigation}) {
+export default function HomeScreen({ route, navigation }) {
+
+    const getAllKeys = async () => {
+        let keys = []
+        try {
+            keys = await AsyncStorage.getAllKeys()
+        } catch (e) {
+            // read key error
+        }
+
+        console.log(keys)
+    }
+
     return (
         <View style={styles.container}>
             <TouchableOpacity
-            style={styles.floatButton} 
-            onPress={()=>{
-                navigation.navigate("Add")
-            }}>
-                <AntDesign 
-                name="plus" 
-                size={30} 
-                color="white" />
+                style={styles.floatButton}
+                onPress={() => {
+                    navigation.navigate("Add")
+                }}>
+                <AntDesign
+                    name="plus"
+                    size={30}
+                    color="white" />
             </TouchableOpacity>
+            <Button onPress={() => { getAllKeys() }}>
+                <Text>Save</Text>
+            </Button>
         </View>
     )
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-  listItem: {
-    flexDirection: "row",
-    padding: 20
-  },
-  iconContainer: {
-    width: 50,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#B83227",
-    borderRadius: 100
-  },
-  contactIcon: {
-    fontSize: 28,
-    color: "#fff"
-  },
-  infoContainer: {
-    flexDirection: "column"
-  },
-  infoText: {
-    fontSize: 16,
-    fontWeight: "400",
-    paddingLeft: 10,
-    paddingTop: 2
-  },
-  floatButton: {
-    borderWidth: 1,
-    borderColor: "rgba(0,0,0,0.2)",
-    alignItems: "center",
-    justifyContent: "center",
-    width: 60,
-    position: "absolute",
-    bottom: 40,
-    right: 10,
-    height: 60,
-    backgroundColor: "#B83227",
-    borderRadius: 100
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "#fff"
+    },
+    listItem: {
+        flexDirection: "row",
+        padding: 20
+    },
+    iconContainer: {
+        width: 50,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#B83227",
+        borderRadius: 100
+    },
+    contactIcon: {
+        fontSize: 28,
+        color: "#fff"
+    },
+    infoContainer: {
+        flexDirection: "column"
+    },
+    infoText: {
+        fontSize: 16,
+        fontWeight: "400",
+        paddingLeft: 10,
+        paddingTop: 2
+    },
+    floatButton: {
+        borderWidth: 1,
+        borderColor: "rgba(0,0,0,0.2)",
+        alignItems: "center",
+        justifyContent: "center",
+        width: 60,
+        position: "absolute",
+        bottom: 40,
+        right: 10,
+        height: 60,
+        backgroundColor: "#B83227",
+        borderRadius: 100
+    }
 });
