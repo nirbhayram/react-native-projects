33953f04ef0d4e09cd89f3763d5cd03f0b577f02
Nirbhay <da201501068@gmail.com>
2020-07-17 01:00:02 +0530

    created reducer and store

--- rn-redux-basics/App.js
+++ rn-redux-basics/App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+var initialState = {
+	myCounter: 0
+}
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case "INC_COUNTER":
+			return { ...state, myCounter: state.myCounter + 1 }
+		case "DEC_COUNTER":
+			return { ...state, myCounter: state.myCounter - 1 }
+	}
+	return state;
+}
+
+const store = createStore(reducer)
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
